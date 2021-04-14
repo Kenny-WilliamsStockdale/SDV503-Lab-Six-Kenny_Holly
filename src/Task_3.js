de56eb87d8eabbc16b99a707c44814701215b97e
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-04-14 20:29:14 +1200

    fixed comment errors

--- src/Task_3.js
+++ src/Task_3.js
@@ -7,10 +7,10 @@
 //return a number count for likes and dislikes
 
 function counts (obj) { // creating a function that will pass an object
-    let likes = obj.likes // declaring a variable likes to = obj.likes (using . notation to get a property of the object)
-    let dislikes = obj.dislikes // declaring a variable dislikes to = obj.dislikes (using . notation to get a property of the object)
+    let likes = obj.likes // declaring a variable likes value of = obj.likes (using . notation to get a property of the object)
+    let dislikes = obj.dislikes // declaring a variable dislikes value of = obj.dislikes (using . notation to get a property of the object)
     likes -= dislikes // using subtraction assignment to subtract dislikes from likes then assigning the sum value to likes
     return likes // returning likes variable
 }
 
-console.log(counts({likes: 20, dislikes: 15})) // console logging the function and object arguments to pass through the function.
+console.log(counts({likes: 20, dislikes: 15})) // printing the function and object arguments to pass through the function.
