37cc3f3e2abe0c1a547b02bd42904a84f22fb377
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-04-14 20:22:20 +1200

    added code to Task_3

---
+++ src/Task_3.js
@@ -0,0 +1,16 @@
+//TODO TASK_3:
+//make a function 
+//make an object
+//input properties
+//input data to properties
+//subtract dislikes from likes
+//return a number count for likes and dislikes
+
+function counts (obj) { // creating a function that will pass an object
+    let likes = obj.likes // declaring a variable likes to = obj.likes (using . notation to get a property of the object)
+    let dislikes = obj.dislikes // declaring a variable dislikes to = obj.dislikes (using . notation to get a property of the object)
+    likes -= dislikes // using subtraction assignment to subtract dislikes from likes then assigning the sum value to likes
+    return likes // returning likes variable
+}
+
+console.log(counts({likes: 20, dislikes: 15})) // console logging the function and object arguments to pass through the function.
