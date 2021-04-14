62154c8d0e4e7259fe876f460e3a749d76097cc9
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-04-14 20:44:52 +1200

    fixed t1 spelling and basic layout

--- src/Task_1.js
+++ src/Task_1.js
@@ -9,18 +9,18 @@
 function odd (x) { //declared a function 
     let arr = [] //made an empty array
     for (let i = 0; i <= x; i++) {  //made for loop to iterate number data and increment by 1 if i is less than X
-   if (i%2 !== 0) { //if statement, if, i has a remander of two, strict inequailty, checks the two operants are not equal and then checking the remainder is zero
+   if (i%2 !== 0) { //if statement, if, i has a remainder of two, strict inequality, checks the two operants are not equal and then checking the remainder is zero
        arr.push(i) //if the above statement number is not equate to zero, then push to the array
-     }
-    //console.log(arr) learing by Debugging
+    }
+    //console.log(arr) learning by Debugging
     } 
-    let result = 0 //declearing variable with value zero
+    let result = 0 //declaring variable with value zero
     for (let a = 0; a < arr.length; a++) { //made a for loop for iterating numbers to find sum
         result += arr[a] //getting result using addition assignment operator which will add both of the results and array together basically smashing them together, to make a sum.
 
     }
-return result
+    return result
 }
-console.log(odd(13)) //console logging argument which is 5 in this case to bubild our array and find the sum.
+console.log(odd(13)) //console logging argument which is 5 in this case to build our array and find the sum.
 
 
